847af4aa4cb31698ac0050caba43b01a8ffb94e6
Mariusz Daroch <mariusz.daroch21.27@gmail.com>
2021-01-13 22:14:03 +0100

    Accesory delate not used setter

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,40 +2,36 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+    private int[] numbers = new int[12];
 
-	private int total = -1;
+    private int total = -1;
 
-	public void countIn(int in) {
-		if (!isFull())
-			numbers[++total] = in;
-	}
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
 
-	public boolean callCheck() {
-		return total == -1;
-	}
+    public boolean callCheck() {
+        return total == -1;
+    }
 
-	public boolean isFull() {
-		return total == 11;
-	}
+    public boolean isFull() {
+        return total == 11;
+    }
 
-	protected int peekaboo() {
-		if (callCheck())
-			return -1;
-		return numbers[total];
-	}
+    protected int peekaboo() {
+        if (callCheck())
+            return -1;
+        return numbers[total];
+    }
 
-	public int countOut() {
-		if (callCheck())
-			return -1;
-		return numbers[total--];
-	}
+    public int countOut() {
+        if (callCheck())
+            return -1;
+        return numbers[total--];
+    }
 
-	public void setTotal(int total) {
-		this.total = total;
-	}
-
-	public int getTotal() {
-		return total;
-	}
+    public int getTotal() {
+        return total;
+    }
 }
